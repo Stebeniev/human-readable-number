74fdb2fc5e68c9700637effb0902125c19ec71bf
Dmitriy Stebenev <dm.stebenev@gmail.com>
2023-08-11 15:23:28 +0300

    init: index.js

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
-module.exports = function toReadable (number) {
-  
+module.exports =    function toReadable(number) {
+
+    const units = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine', 'ten','eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    // const teens = ['', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen'];
+    const tens = ['', '',  'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'];
+    const hundreds = ['', 'one hundred', 'two hundred', 'three hundred', 'four hundred', 'five hundred', 'six hundred', 'seven hundred', 'eight hundred', 'nine hundred'];
+
+
+
+    if (number < 20) {
+        return units[number];
+    } else if (number < 100) {
+        return tens[Math.trunc(number / 10)] + (number % 10 === 0 ? '': ' ' + units[number % 10]);
+    } else {
+        return hundreds[Math.floor(number / 100)] + (number % 100 === 0 ? '':' ' + toReadable(number % 100));
+    }
 }
+
+
+
+
+
+
